a861181bcf87fa2a682fa886534832fb1ab1e406
Sébastien Pouliot <spouliot@tux.sebastienpouliot.com>
2017-10-25 14:04:17 -0400

    inital commit

---
+++ app/src/main/java/com/example/spouliot/layoutexemples/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.spouliot.layoutexemples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.responsive1_layout);
+    }
+}
